9e6ac71d0f1848b3dfbfe2e044138c0441e5591b
fake30201-star <fake30201@gmail.com>
2026-08-17 02:06:42 +0300

    Refactor service worker to cache shell files

--- sw.js
+++ sw.js
@@ -1,16 +1,29 @@
 const CACHE = 'masrofaty-v1';
-self.addEventListener('install', e => { 
-  self.skipWaiting(); 
+const SHELL = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(SHELL).catch(()=>{}))
+  );
 });
-self.addEventListener('activate', e => { 
-  self.clients.claim(); 
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
-self.addEventListener('fetch', e => {
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.open(CACHE).then(async cache => {
+    caches.open(CACHE).then(async (cache) => {
       try {
         const res = await fetch(e.request);
-        if (e.request.method === 'GET') cache.put(e.request, res.clone());
+        cache.put(e.request, res.clone());
         return res;
       } catch (err) {
         const cached = await cache.match(e.request);
